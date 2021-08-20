f7bc9639b6eb6c12fc317e2187ccb07860b7b62e
Galin <dev@galin.cc>
2021-08-20 17:39:07 +0200

    JUP-50 Refactor the module into a class

--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,88 @@
 import * as Joi from 'joi'
 import { Stats } from './types';
 
-let stats: Stats = {}
+export default class StatsService {
+  private stats: Stats
+  private statsKeys
 
-/**
- * Sets a given stat key with a given value
- *
- * @param key
- * @param value
- */
-function setStat(key, value): void {
-  if (!key) {
-    throw new Error('Stats key not provided')
+  constructor() {
+    this.stats = {}
+    // The object holding the stats keys for the service utilizing this lib
+    this.statsKeys = {}
   }
 
-  const { value: validValue, error } = Joi.number().required().validate(value)
-  if (error) {
-    throw new Error(error.message)
+  /**
+   * Sets a given stat key with a given value
+   *
+   * @param key
+   * @param value
+   */
+  setStat(key, value): void {
+    if (!key) {
+      throw new Error('Stats key not provided')
+    }
+
+    const { value: validValue, error } = Joi.number().required().validate(value)
+    if (error) {
+      throw new Error(error.message)
+    }
+    this.stats[key] = validValue
   }
-  stats[key] = validValue
-}
 
-/**
- * Returns the value of the given stat. If not present, return 0.
- *
- * @param key
- * @return {number}
- */
-function getStat(key): number {
-  if (!key) {
-    throw new Error('Stats key not provided')
+  /**
+   * Returns the value of the given stat. If not present, return 0.
+   *
+   * @param key
+   * @return {number}
+   */
+  getStat(key): number {
+    if (!key) {
+      throw new Error('Stats key not provided')
+    }
+    return this.stats[key] || 0
   }
-  return stats[key] || 0
-}
 
-/**
- * Increments the stat value. If the stat key isn't set yet, set it.
- *
- * @param key
- */
-function incrementStat(key): void {
-  if (!key) {
-    throw new Error('Stats key not provided')
+  /**
+   * Increments the stat value. If the stat key isn't set yet, set it.
+   *
+   * @param key
+   */
+  incrementStat(key): void {
+    if (!key) {
+      throw new Error('Stats key not provided')
+    }
+    this.stats[key] = (this.stats[key] || 0) + 1
   }
-  stats[key] = (stats[key] || 0) + 1
-}
 
-/**
- * Decrements the stat value. If the stat key isn't set yet, set it.
- *
- * @param key
- */
-function decrementStat(key): void {
-  if (!key) {
-    throw new Error('Stats key not provided')
+  /**
+   * Decrements the stat value. If the stat key isn't set yet, set it.
+   *
+   * @param key
+   */
+  decrementStat(key): void {
+    if (!key) {
+      throw new Error('Stats key not provided')
+    }
+    this.stats[key] = (this.stats[key] || 0) - 1
   }
-  stats[key] = (stats[key] || 0) - 1
-}
 
-/**
- * Return the `stats` object, nested in an object.
- * This schema is required by the stats-collecting agent we use.
- * More info here: https://darcs.atlassian.net/wiki/spaces/DEVELOPMEN/pages/5505224/Grafana
- *
- * @return {{stats: {}}}
- */
-function getAllStats(): { stats: Stats } {
-  return { stats }
-}
+  /**
+   * Return the `stats` object, nested in an object.
+   * This schema is required by the stats-collecting agent we use.
+   * More info here: https://darcs.atlassian.net/wiki/spaces/DEVELOPMEN/pages/5505224/Grafana
+   *
+   * @return {{stats: {}}}
+   */
+  getAllStats(): { stats: Stats } {
+    return { stats: this.stats }
+  }
 
-/**
- * The object holding the stats keys for the service utilizing this lib
- */
-const statsKeys = {
-  'bfxSync': 'bfx_sync_total_candles_synced'
-}
-
-/**
- * Clears the `stats` in-mem object. Used for automated tests only!
- *
- * @private
- */
-function _clearStats(): void {
-  stats = {}
-}
-
-module.exports = {
-  setStat,
-  getStat,
-  incrementStat,
-  decrementStat,
-  getAllStats,
-  statsKeys,
-
-  // used for automated tests
-  _clearStats
+  /**
+   * Clears the `stats` in-mem object. Used for automated tests only!
+   *
+   * @private
+   */
+  _clearStats(): void {
+    this.stats = {}
+  }
 }
