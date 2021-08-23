432786a7c0c25941799d05bbe54eeca03e9e0b54
Galin <dev@galin.cc>
2021-08-23 13:35:20 +0200

    JUP-50 Update tests according to Singleton implementation

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,54 +1,61 @@
-const statsService = require('../src/index')
+import StatsService from '../src/index';
 
 describe('stats service', function () {
-  const testStatKey = 'test_stat'
+  const testStatKey = 'test_stat';
+  const statsService = new StatsService('stats_service', ['test_stat']);
 
   beforeEach(() => {
-    statsService._clearStats()
+    statsService._clearStats();
+  })
+
+  it('should be identical instance at subsequent initializations, proving the Singleton pattern', function () {
+    const anotherStatsServiceInst = new StatsService('some_new_service', ['other_stats_yooo']);
+
+    anotherStatsServiceInst.getAllStats().should.deep.equal(statsService.getAllStats());
   })
 
   it('should set and get stat', function () {
-    statsService.setStat(testStatKey, 123)
-    const stat = statsService.getStat(testStatKey)
+    statsService.setStat(testStatKey, 123);
+    const stat = statsService.getStat(testStatKey);
 
-    stat.should.equal(123)
+    stat.should.equal(123);
   })
 
   it('should increment a stat after its been set and get it', function () {
-    statsService.setStat(testStatKey, 123)
-    statsService.incrementStat(testStatKey)
-    const stat = statsService.getStat(testStatKey)
+    statsService.setStat(testStatKey, 123);
+    statsService.incrementStat(testStatKey);
+    const stat = statsService.getStat(testStatKey);
 
-    stat.should.equal(124)
+    stat.should.equal(124);
   })
 
   it('should set a new stat when not present when trying to increment the stat', function () {
-    statsService.incrementStat(testStatKey)
-    const stat = statsService.getStat(testStatKey)
+    statsService.incrementStat(testStatKey);
+    const stat = statsService.getStat(testStatKey);
 
-    stat.should.equal(1)
+    stat.should.equal(1);
   })
 
   it('should decrement an already set stat', function () {
-    statsService.setStat(testStatKey, 123)
-    statsService.decrementStat(testStatKey)
-    const stat = statsService.getStat(testStatKey)
+    statsService.setStat(testStatKey, 123);
+    statsService.decrementStat(testStatKey);
+    const stat = statsService.getStat(testStatKey);
 
-    stat.should.equal(122)
+    stat.should.equal(122);
   })
 
   it('should set a new stat when not present when trying to decrement the stat', function () {
-    statsService.decrementStat(testStatKey)
-    const stat = statsService.getStat(testStatKey)
+    statsService.decrementStat(testStatKey);
+    const stat = statsService.getStat(testStatKey);
 
-    stat.should.equal(-1)
+    stat.should.equal(-1);
   })
 
   it('should get all stats nested in a "stats" object', function () {
-    statsService.setStat(testStatKey, 1)
-    statsService.setStat(`${testStatKey}_A`, 2)
-    statsService.setStat(`${testStatKey}_B`, 3)
-    const stats = statsService.getAllStats()
+    statsService.setStat(testStatKey, 1);
+    statsService.setStat(`${testStatKey}_A`, 2);
+    statsService.setStat(`${testStatKey}_B`, 3);
+    const stats = statsService.getAllStats();
 
     stats.should.deep.equal({
       stats: {
@@ -56,6 +63,6 @@ describe('stats service', function () {
         [`${testStatKey}_A`]: 2,
         [`${testStatKey}_B`]: 3
       }
-    })
+    });
   })
 })
